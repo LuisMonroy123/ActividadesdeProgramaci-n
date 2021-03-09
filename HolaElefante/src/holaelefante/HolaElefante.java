a6ae8a37e246051aa1726b0ebdf02c8a2e5d5875
LuisMonroy123 <62566984+LuisMonroy123@users.noreply.github.com>
2021-03-08 19:47:38 -0600

    Tarea: programa hola

---
+++ HolaElefante/src/holaelefante/HolaElefante.java
@@ -0,0 +1,21 @@
+//En esta parte puedes importar paquetes
+package holaelefante;
+
+/**
+ *
+ * @author Monroy Vazquez Luis Antonio
+ */
+//Esto es una clase publica 
+public class HolaElefante {
+
+    /**
+     * @param args Esta lina sirve para comentar el codigo
+     */
+    
+    //El main es la clase que permite ejecutar los codigos
+    public static void main(String[] args) {
+        //Metodo para imprimir un mensaje en el sistema
+        System.out.println("Hola elefante");
+    }
+    
+}
